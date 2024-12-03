d00e3d8034ce6adf6223a1c6c6724fdc86aa2cf4
chamithjeewantha <139670985+chamithjeewantha@users.noreply.github.com>
2024-12-03 17:22:19 +0000

    complete product component

--- ica01/src/components/Product.js
+++ ica01/src/components/Product.js
@@ -1,7 +1,27 @@
 import '../assets/CSS/layout.css';
 import { useState } from 'react'
 export default function Product({flowerData,addCart}){
-    
+    const [ quantity, setQuantity ] = useState()
+    const product = {
+        name:"",
+        quantity:"",
+        sum:""
+    }
+
+    function handleFlower(value) {
+        setQuantity(value)
+    }
+
+    function addToCart() {
+
+        product.name = flowerData.name
+        product.quantity = quantity
+        product.sum = quantity * flowerData.price
+
+        if(product) {
+            addCart(product)
+        }
+    }
     return(
         <div className="grid-item">
 
@@ -12,9 +32,9 @@ export default function Product({flowerData,addCart}){
                     <h5 class="card-title">Price:{flowerData.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:{flowerData.qty}</label>
-                        <input type="number" id="quantity" name="quantity" />
+                        <input type="number" id="quantity" name="quantity" onChange={(e) => {handleFlower(e.target.value)}}/>
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button"onClick={() => {addToCart()}}>Add to Cart</button>
                 </div>
             </div>
         </div>
